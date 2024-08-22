23bb9fdb636709d64c8d090a094efdbad01075cc
Jonathan Zhang <99jzhang@gmail.com>
2024-08-22 01:13:47 -0400

    Clean slate - cleared out Vite template

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,13 @@ import './App.css'
 import ReactionArea from './Components/ReactionArea'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("");
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>YourReactionTime</h1>
-      <h2>A reaction time testing website with customizable elements</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR.
-        </p>
-      </div>
       <div>
         <ReactionArea />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
